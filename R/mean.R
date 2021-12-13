6d0ff61f5441b8e74c3af8106883ef6a72607a53
Emilostuff <emilostuff@gmail.com>
2021-12-13 13:27:45 +0100

    new mean ci

--- R/mean.R
+++ R/mean.R
@@ -1,16 +1,3 @@
-#' Mean Confidence Interval Deviation
-#'
-#' @param x a data vector
-#' @param c confidence (between 0..1)
-#'
-#' @return
-#' @export
-#'
-#' @examples
-mean_ci_dev <- function(x, c = 0.95) {
-  qt((1+c)/2, df=length(x)-1)*std_error(x)
-}
-
 
 #' Mean Confidence Interval
 #'
@@ -22,5 +9,25 @@ mean_ci_dev <- function(x, c = 0.95) {
 #'
 #' @examples
 mean_ci <- function(x, c = 0.95) {
-  c(mean(x)-mean_ci_dev(x, c), mean(x)+mean_ci_dev(x, c))
+  mean_ci_nv(mean(x), sd(x), length(x), c)
 }
+
+
+#' Mean Confidence Interval - No Vector
+#'
+#' @param mu mean
+#' @param s standard deviation
+#' @param n degrees of freedom
+#' @param c confidence (between 0..1)
+#'
+#' @return
+#' @export
+#'
+#' @examples mean_ci_nv(mu = -0.1181818, s = 0.05884899, n = 22)
+mean_ci_nv <- function(mu, s, n, c = 0.95) {
+  mu + c(-1, 1) * qt((1+c)/2, df = n - 1) * s / sqrt(n)
+}
+
+
+
+
